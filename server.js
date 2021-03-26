f4b85da4319bf2a879ae5ecaf2fab239bf1ef57e
El Omar Sami <elomar.sami@skarabee.com>
2021-03-27 00:34:39 +0100

    frontend setup with heroku

--- server.js
+++ server.js
@@ -31,9 +31,8 @@ app.prepare()
 			return handle(req, res)
 		});
 
-    server.listen(3000, (err) => {
+    server.listen(process.env.PORT || 3000, (err) => {
 			if (err) throw err
-			console.log('> Ready on http://localhost:3000')
 		});
   })
   .catch(ex => {
