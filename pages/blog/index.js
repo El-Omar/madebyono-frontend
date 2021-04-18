563ef42c107777d10709d1858acc35a06fbf5b24
El Omar Sami <elomar.sami@skarabee.com>
2021-04-18 23:56:55 +0200

    blog overview page loading fix

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -40,14 +40,14 @@ const Blog = () => {
         <span className="no-results-found">Failed to load the shop</span>
       </div>}
 
-      {loading && <div className="col-row">
+      {loading && <div className="container"><div className="col-row">
         <div className="w-50">
           <div className="loading-box"></div>
         </div>
         <div className="w-50">
           <div className="loading-box"></div>
         </div>
-      </div>}
+      </div></div>}
 
       {!error && !loading && <>
         <BlogList articles={data.blogs} />
