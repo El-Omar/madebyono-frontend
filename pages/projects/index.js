af8b348c2210f70d750f12a01a336abaa9fec103
Yoshi De Schrijver <83425038+YoshiDeSchrijver@users.noreply.github.com>
2021-05-03 16:38:31 +0200

    Update index.js

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -39,8 +39,8 @@ const Projects = () => {
       <div className="container">
         <header className="page__heading">
           <h1>
-            Collabs, Projects<br/>
-             &amp; High-end Designs.
+            Collabs, Projects <br/>
+            &amp; High-end Designs.
           </h1>
         </header>
 
