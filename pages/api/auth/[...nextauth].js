e2a484528b22cd665cc79e54509df3689cf3d34d
El Omar Sami <elomar.sami@skarabee.com>
2021-05-06 00:24:16 +0200

    db connection issues attempt

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -23,7 +23,18 @@ const options = {
   // pages: {
   //   signIn: '/signin',
   // },
-  database: process.env.DATABASE_URL
+  database: {
+    type: "mongodb",
+    // port: 26257,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    // ssl: {
+    //   rejectUnauthorized: false,
+    //   ca: fs.readFileSync('/path/to/server-certificates/root.crt').toString()
+    // },
+  }
 };
 
 export default (req, res) => NextAuth(req, res, options);
