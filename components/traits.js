94e3e4e66835cb9fb5892c98dfd9a28e3546277c
Yoshi De Schrijver <83425038+YoshiDeSchrijver@users.noreply.github.com>
2021-06-08 01:11:46 +0200

    Update traits.js

--- components/traits.js
+++ components/traits.js
@@ -3,18 +3,18 @@ export default () => {
   
   traits.set(1, "A Right-Brained Nerd");
   traits.set(3, "INTJ-A Personality");
-  traits.set(5, "I'm Great at Logo Design");
-  traits.set(7, "An Introvert Wannabe Extravert");
-  traits.set(8, "I Align brands to visions");
-  traits.set(9, "I Have A Shop For Startups");
+  traits.set(5, "Great at Logo Design");
+  traits.set(7, "Introvert Wannabe Extravert");
+  traits.set(8, "Aligning Brands to Visions");
+  traits.set(9, "Explore my Startup-Shop");
   traits.set(11, "Master in Photoshop");
   traits.set(12, "Not Just An IT Guy");
-  traits.set(13, "AKA The Creative Sensei");
+  traits.set(13, "AKA Creative Sensei");
   traits.set(14, "A Business, Man");
-  traits.set(15, "A Very Popular Loner");
-  traits.set(17, "Shop Discount: EASTEREGG10");
+  traits.set(15, "A Popular Loner");
+  traits.set(17, "Discount: EASTEREGG10");
   traits.set(18, "Part Time Grocery Clerk");
-  traits.set(19, "A Guy Chasing His Dreams");
+  traits.set(19, "A Guy Chasing Dreams");
   traits.set(21, "Art Junky & Musician");
   traits.set(23, "Analytical Designer");
   traits.set(25, "The Imagineer");
