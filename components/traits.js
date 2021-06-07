65ee14b88ecf28815fc68d1fba0575bc87ab14d8
Yoshi De Schrijver <83425038+YoshiDeSchrijver@users.noreply.github.com>
2021-06-07 23:25:37 +0200

    Update traits.js

--- components/traits.js
+++ components/traits.js
@@ -2,21 +2,21 @@ export default () => {
   const traits = new Map();
   
   traits.set(1, "A Right-Brained Nerd");
-  traits.set(3, "Check out my shop!");
+  traits.set(3, "One Of ThosePeople Trusting The Universe");
   traits.set(5, "I'm Great at Logo Design");
-  traits.set(7, "The Silent Speaker");
+  traits.set(7, "An Introvert Learning To Be Extravert");
   traits.set(8, "I Align brands to visions");
-  traits.set(9, "A Renaissance Man");
+  traits.set(9, "My Shop's Full Of Handy Stuff");
   traits.set(11, "Master in Photoshop");
-  traits.set(12, "Not Just the IT Guy");
-  traits.set(13, "The Creative Sensei");
+  traits.set(12, "Not Just An IT Guy");
+  traits.set(13, "AKA The Creative Sensei");
   traits.set(14, "A Business, Man");
-  traits.set(15, "A Popular Loner");
-  traits.set(17, "A Tea Drinker");
-  traits.set(18, "A Freelancer");
-  traits.set(19, "A regular guy chasing his dreams");
-  traits.set(21, "An Art Junky");
-  traits.set(23, "A Business Buddhist");
+  traits.set(15, "A Very Popular Loner");
+  traits.set(17, "I Drink Both Coffee And Tea");
+  traits.set(18, "Part Time Grocery Clerk");
+  traits.set(19, "Just A Regular Guy Chasing His Dreams");
+  traits.set(21, "An Art Junky & Musician");
+  traits.set(23, "Actually An Analytical Designer");
   traits.set(25, "The Imagineer");
   
   return traits;
