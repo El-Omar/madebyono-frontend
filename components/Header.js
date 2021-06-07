30f1291d2c3092ea5abb0cefaeab3253464ab8e5
Yoshi De Schrijver <83425038+YoshiDeSchrijver@users.noreply.github.com>
2021-06-07 22:44:21 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -75,7 +75,7 @@ const Header = ({ setBodyOverlay }) => {
                     </Link>
                   </li>
                   <li className="navigation__link page__link-shop">
-                    <Link href="http://startup-kits.webflow.io/" target="_blank" rel="noopener noreferrer">
+                    <Link href="http://startup-kits.webflow.io/" target="_blank">
                       <a className="link-wrap">
                         <span className="link-title color--yellow">Handy</span> <span className="page-name">Shop</span>
                       </a>
