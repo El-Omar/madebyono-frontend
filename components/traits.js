01bfc5d3c656f3d85932a2a34b77455135f6d88d
Yoshi De Schrijver <83425038+YoshiDeSchrijver@users.noreply.github.com>
2021-06-07 23:11:43 +0200

    Update traits.js

--- components/traits.js
+++ components/traits.js
@@ -1,20 +1,20 @@
 export default () => {
   const traits = new Map();
   
-  traits.set(1, "A Left-Brained Nerd");
-  traits.set(3, "The Goal Getter");
-  traits.set(5, "Your Friend");
+  traits.set(1, "A Right-Brained Nerd");
+  traits.set(3, "Check out my shop!");
+  traits.set(5, "I'm Great at Logo Design");
   traits.set(7, "The Silent Speaker");
-  traits.set(8, "A Dreamer");
+  traits.set(8, "I Align brands to visions");
   traits.set(9, "A Renaissance Man");
-  traits.set(11, "Devotion Itself");
+  traits.set(11, "Master in Photoshop");
   traits.set(12, "Not Just the IT Guy");
-  traits.set(13, "A Freelance Creative");
+  traits.set(13, "The Creative Sensei");
   traits.set(14, "A Business, Man");
   traits.set(15, "A Popular Loner");
   traits.set(17, "A Tea Drinker");
-  traits.set(18, "A Creative Sensei");
-  traits.set(19, "Emotionally Aware");
+  traits.set(18, "A Freelancer");
+  traits.set(19, "A regular guy chasing his dreams");
   traits.set(21, "An Art Junky");
   traits.set(23, "A Business Buddhist");
   traits.set(25, "The Imagineer");
